c8af0c4958dd0c9aa94a65110ec7d5ff6ee4dabf
Chetanthakur03 <119585273+Chetanthakur03@users.noreply.github.com>
2023-02-24 16:03:23 +0530

    Update ping.py

--- altron/plugins/ping.py
+++ altron/plugins/ping.py
@@ -8,7 +8,7 @@ from config import *
 @Client.on_message(filters.command(["ping"], ["/", ".", "!"]) & filters.user(SUDO_USERS))
 async def ping(Client, message):
     start = datetime.now()
-    loda = await message.reply_text("» __ᴀʟᴛʀᴏɴ__")
+    loda = await message.reply_text("» __ALpha__")
     end = datetime.now()
     mp = (end - start).microseconds / 1000
     await loda.edit_text(f"__🤖 ᴘɪɴɢ__\n» `{mp} ms`")
