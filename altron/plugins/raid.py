899469a4cadae31b32d5f7490bef9244b82cf124
DJ <119585273+Chetanthakur03@users.noreply.github.com>
2023-08-11 05:28:05 +0530

    Update raid.py

--- altron/plugins/raid.py
+++ altron/plugins/raid.py
@@ -14,7 +14,7 @@ async def dmraid(xspam: Client, e: Message):
           ok = await xspam.get_users(TheAltronX[1])
           id = ok.id
           if int(id) in VERIFIED_USERS:
-                text = f"`ᴠᴇʀɪғɪᴇᴅ ʙʏ ᴀʟᴘʜᴀ x`"
+                text = f"`ᴠᴇʀɪғɪᴇᴅ ʙʏ ᴍᴇ`"
                 await e.reply_text(text)
           elif int(id) in SUDO_USERS:
                 text = f"`ᴛʜɪs ᴘᴇʀsᴏɴ ɪs ᴍʏ sᴜᴅᴏ ᴜsᴇʀ`"
@@ -33,7 +33,7 @@ async def dmraid(xspam: Client, e: Message):
           ok = await xspam.get_users(user_id)
           id = e.chat.id
           if int(id) in VERIFIED_USERS:
-                text = f"`ᴠᴇʀɪғɪᴇᴅ ʙʏ ᴀʟᴘʜᴀ x`"
+                text = f"`ᴠᴇʀɪғɪᴇᴅ ʙʏ ᴍᴇ`"
                 await e.reply_text(text)
           elif int(id) in SUDO_USERS:
                 text = f"`ᴛʜɪs ᴘᴇʀsᴏɴ ɪs ᴍʏ sᴜᴅᴏ ᴜsᴇʀ`"
